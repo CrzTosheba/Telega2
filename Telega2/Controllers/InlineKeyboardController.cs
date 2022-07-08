64ce0621b665e4b806cb16f6bd6b01fb6f5b621d
CrzTosheba <fugu@list.ru>
2022-07-08 16:10:05 +0300

    Добавьте файлы проекта.

---
+++ Telega2/Controllers/InlineKeyboardController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+
+namespace Telega2.Controllers
+{
+    public class InlineKeyboardController
+    {
+        private readonly ITelegramBotClient _telegramClient;
+
+        public InlineKeyboardController(ITelegramBotClient telegramBotClient)
+        {
+            _telegramClient = telegramBotClient;
+        }
+        // InlineKeyboardController.cs
+        public async Task Handle(CallbackQuery? callbackQuery, CancellationToken ct)
+        {
+            Console.WriteLine($"Контроллер {GetType().Name} обнаружил нажатие на кнопку");
+
+            _ = await _telegramClient.SendTextMessageAsync(callbackQuery.From.Id, $"Обнаружено нажатие на кнопку", cancellationToken: ct);
+        }
+    }
+}
---
+++ Telega2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Telega2.Controllers;
+using Telegram.Bot;
+using Telega2;
+using UtilityBot;
+
+namespace Telega2
+{
+    static class Program
+    {
+        public static async Task Main()
+        {
+            Console.OutputEncoding = Encoding.Unicode;
+
+            var host = new HostBuilder()
+                .ConfigureServices((hostContext, services) => ConfigureServices(services))
+                .UseConsoleLifetime()// Позволяет поддерживать приложение активным в консоли
+                .Build();
+
+            Console.WriteLine("Стартуем!!!");
+            // Запускаем сервис
+            await host.RunAsync();
+            Console.WriteLine("Сервис остановлен");
+        }
+
+        static void ConfigureServices(IServiceCollection services)
+        {
+            // Подключаем контроллеры сообщений и кнопок
+       
+            services.AddTransient<TextMessageController>();
+          
+            services.AddSingleton<ITelegramBotClient>(provider => new TelegramBotClient("5344280901:AAGNsq45lXc7u1SZdzDVrLHid8yVVA2u5TU"));
+            //services.AddHostedService<Bot>();
+            services.AddHostedService<BotText>();
+        }
+    }
+}
